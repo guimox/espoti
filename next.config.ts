a286aa29d7ecf982ea983a40ce7418bad105e0de
Guilherme Lopes <guilhermxlopes@gmail.com>
2025-03-29 07:45:37 -0300

    chore: infra content and code/config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   try {
-    const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
+    const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
     setupDevPlatform();
   } catch (error) {
-    console.error("Failed to set up Cloudflare dev platform:", error);
+    console.error('Failed to set up Cloudflare dev platform:', error);
   }
 }
 
